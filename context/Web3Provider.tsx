4f86113f2433e5fd3480febe68e0981d5208a992
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-09-08 12:32:36 +0300

    remove code

--- context/Web3Provider.tsx
+++ context/Web3Provider.tsx
@@ -196,10 +196,11 @@ export const Web3Provider = ({ children }: { children: ReactNode }) => {
       },
     ];
 
-    const litNodeClient = await connectToLitNodes();
-    const result = await getSessionSignatures(litNodeClient, mintedPkp as MintedPkp, user?.id.toString() as string);
-    console.log(result);
+    // const litNodeClient = await connectToLitNodes();
+    // const result = await getSessionSignatures(litNodeClient, mintedPkp as MintedPkp, user?.id.toString() as string);
+    // console.log(result);
     const sig = await getPkpSessionSigs(user as unknown as TelegramUser, mintPkp as any);
+    console.log(sig);
     // const decodedMessage = await decryptWithLit(lit as ILitNodeClient, ciphertext, dataToEncryptHash, accessControlConditions, "ethereum");
     // if (type === "file") {
     //   const uintArray = decodeb64(decodedMessage);
